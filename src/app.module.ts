c200b311ba65126cb84b17f209a1bbfa7a417b0a
Ko-GyeongTae <kokt0203@naver.com>
2022-06-25 15:18:48 +0900

    Chore: domain init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { ReportModule } from './report/report.module';
+import { NoticeModule } from './notice/notice.module';
+import { FileModule } from './file/file.module';
+import { ProjectModule } from './project/project.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, AdminModule, ReportModule, NoticeModule, FileModule, ProjectModule],
   controllers: [AppController],
   providers: [AppService],
 })
